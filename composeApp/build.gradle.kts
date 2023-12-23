e080cbef4f81376a565b93dbb4f84aef1cb8c57e
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2023-12-23 07:58:58 +0100

    Add missing plugin dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.sqlDelight)
 }
 
 kotlin {
